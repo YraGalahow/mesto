1a73d347b2a20f4c9377baee265b08f7fd6ab712
Ujra Galahow <galaxowa@list.ru>
2020-05-22 22:57:06 +0300

    Проектная работа

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 let popup = document.querySelector('.popup');
 const popupSubmitButton = document.querySelector('popup__button');
-const profileEditButton = document.querySelector('.profile__Edit-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
-let profileFirstName = document.querySelector('.profile__First-Name');
+let profileFirstName = document.querySelector('.profile__first-name');
 let profileposition = document.querySelector('.profile__position');
-let popupFirstName = document.querySelector('.popup__First-Name');
-let popupposition = document.querySelector('.popup__position');
+let popupFirstName = document.querySelector('.popup_first-name');
+let popupposition = document.querySelector('.popup_position');
 let formElement = document.querySelector('.popup__container');
+
 closePopup();
 
 function openPopup() {
+    popup.classList.add('popup_opened');
     popupFirstName.value = profileFirstName.innerText;
     popupposition.value = profileposition.innerText;
-    popup.classList.add('popup_opened');
+
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
-    popupFirstName.value = profileFirstName.innerText;
-    popupposition.value = profileposition.innerText;
-
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let nameInput = document.querySelector('.popup__First-Name');
-    let jobInput = document.querySelector('.popup__position');
+    popupFirstName.value;
+    popupposition.value;
 
-    nameInput.value;
-    jobInput.value;
+    profileFirstName.textContent = popupFirstName.value;
+    profileposition.textContent = popupposition.value;
 
-    let profileFirstName = document.querySelector('.profile__First-Name');
-    let profileposition = document.querySelector('.profile__position');
-
-
-    profileFirstName.textContent = nameInput.value;
-    profileposition.textContent = jobInput.value;
-
-    popup.classList.remove('popup_opened');
+    closePopup()
 }
 
 profileEditButton.addEventListener('click', openPopup);
