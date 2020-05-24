41fa61c12ac1fafe3ad5b72395fb21353be71264
Russian-Travel <62426156+YraGalahow@users.noreply.github.com>
2020-05-24 18:36:56 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:script/script.js
 let popup = document.querySelector('.popup');
 const popupSubmitButton = document.querySelector('popup__button');
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -35,42 +34,4 @@ profileEditButton.addEventListener('click', openPopup);
 
 popupCloseButton.addEventListener('click', closePopup);
 
-=======
-let popup = document.querySelector('.popup');
-const popupSubmitButton = document.querySelector('popup__button');
-const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__close');
-let profileFirstName = document.querySelector('.profile__first-name');
-let profilePosition = document.querySelector('.profile__position');
-let popupFirstName = document.querySelector('.popup__input_first-name');
-let popupPosition = document.querySelector('.popup__input_position');
-let formElement = document.querySelector('.popup__container');
-
-closePopup();
-
-function openPopup() {
-    popup.classList.add('popup_opened');
-    popupFirstName.value = profileFirstName.innerText;
-    popupPosition.value = profilePosition.innerText;
-
-}
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-
-    profileFirstName.textContent = popupFirstName.value;
-    profilePosition.textContent = popupPosition.value;
-
-    closePopup()
-}
-
-profileEditButton.addEventListener('click', openPopup);
-
-popupCloseButton.addEventListener('click', closePopup);
-
->>>>>>> b982c873a5a195c113fd81d9fae5eb6be938a40c:script.js
 formElement.addEventListener('submit', formSubmitHandler);
